f99b90eec7c7234ac580642ffce8c9c4113c878a
andgarzonmal <andgarzonmal@unal.edu.co>
2022-07-18 15:08:17 -0500

    set provider and redux

--- src/App.js
+++ src/App.js
@@ -4,23 +4,27 @@ import {
   Route,
 } from 'react-router-dom';
 import Header from './Components/Header/header';
+import { Provider } from 'react-redux';
+import store from './configureStore';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div>
-        <Header />
+    <Provider store={store}>
+      <BrowserRouter>
+        <div>
+          <Header />
 
-        <hr />
-        <div className="content">
-          <Routes>
-            <Route path="/" element={<>Missions</>} />
-            <Route path="/Rockets" element={<>Rockets</>} />
-            <Route path="/Profile" element={<>Profile</>} />
-          </Routes>
+          <hr />
+          <div className="content">
+            <Routes>
+              <Route path="/" element={<>Missions</>} />
+              <Route path="/Rockets" element={<>Rockets</>} />
+              <Route path="/Profile" element={<>Profile</>} />
+            </Routes>
+          </div>
         </div>
-      </div>
-    </BrowserRouter>
+      </BrowserRouter>
+    </Provider>
   );
 }
 export default App;
