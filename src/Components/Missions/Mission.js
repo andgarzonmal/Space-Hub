9a8737e8ad7a2341376f9800e7ecc6d664ebdfa4
andgarzonmal <andgarzonmal@unal.edu.co>
2022-07-20 11:24:20 -0500

    add functionality to join Button

--- src/Components/Missions/Mission.js
+++ src/Components/Missions/Mission.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import propTypes from 'prop-types';
 import './Mission.css';
+import { useDispatch } from 'react-redux/es/exports';
+import { joinMission, unJoinMission } from '../../Redux/Mission/Mission';
 
-function Mission({ name, description }) {
+function Mission({
+  name, description, id, reserved,
+}) {
+  const dispatch = useDispatch();
+  // const state = useSelector((state) => state.missions);
+
+  const handleJoin = () => {
+    dispatch(joinMission(id));
+  };
+
+  const handleUnJoin = () => {
+    dispatch(unJoinMission(id));
+  };
+
   return (
     <div className="mission">
       <div className="grid-item">
@@ -14,10 +29,10 @@ function Mission({ name, description }) {
         </p>
       </div>
       <div className="grid-item">
-        <span className="center-items">Not A Member</span>
+        <span className={reserved ? 'leave' : 'join'}>{reserved ? 'Active Member' : 'Not a Member' }</span>
       </div>
       <div className="center-items grid-item">
-        <button type="button">join mission</button>
+        <button type="button" onClick={(id) => (reserved ? handleUnJoin(id) : handleJoin(id))}>join mission</button>
       </div>
     </div>
   );
@@ -28,4 +43,6 @@ export default Mission;
 Mission.propTypes = {
   name: propTypes.string.isRequired,
   description: propTypes.string.isRequired,
+  id: propTypes.string.isRequired,
+  reserved: propTypes.bool.isRequired,
 };
